589ef0a1c1a134a5d5bb7cdc1bcdd3228bcd9505
miridium-electrode <checkerdance@yahoo.com>
2026-02-12 10:34:15 +0700

    add input bar that can hover

--- app/src/main/java/com/example/ahbit/MainActivity.kt
+++ app/src/main/java/com/example/ahbit/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.relocation.BringIntoViewRequester
+import androidx.compose.foundation.relocation.bringIntoViewRequester
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.OutlinedTextField
@@ -24,42 +29,53 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.view.WindowCompat
 import com.example.ahbit.ui.theme.AhbitTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+//        enableEdgeToEdge()
         setContent {
             AhbitTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    TodoPage(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .padding(innerPadding)
-                    )
-                }
+                TodoPage()
             }
         }
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun TodoPage(modifier: Modifier) {
-    Column(
-        modifier = modifier
-    ) {
-        TodoList(
-            itemss = listOf<String>("do laundry", "clean floor", "wash dish"),
-            modifier = Modifier.weight(1f)
-        )
-        TodoAddField()
-    }
+fun TodoPage() {
+    val bringIntoViewRequester = remember { BringIntoViewRequester() }
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            Box(
+                modifier = Modifier
+                    .imePadding()
+                    .bringIntoViewRequester(bringIntoViewRequester)
+                    .padding(8.dp)
+            ) {
+                TodoAddField()
+            }
+        }
+    ) { paddingValues -> TodoList(
+        itemss = listOf("do laundry", "clean house", "do homework"),
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+    ) }
 }
 
 @Composable
