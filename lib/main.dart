c8f4673b8babbc9ceaf93d3cfaee5985c1a15c76
miridium-electrode <checkerdance@yahoo.com>
2026-05-19 08:35:46 +0700

    add simple habit page

--- lib/main.dart
+++ lib/main.dart
@@ -14,54 +14,61 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HabitListPage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class HabitListPage extends StatelessWidget {
+  const HabitListPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text("Ahbit"),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: Column(
+        children: [
+          Habit(habitName: "aaa"),
+          Habit(habitName: 'bbb'),
+          Habit(habitName: 'ccc')
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
+        onPressed: () {},
+        tooltip: 'None',
         child: const Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class Habit extends StatefulWidget {
+  const Habit({super.key, required this.habitName});
+  final String habitName;
+
+  @override
+  State<Habit> createState() => _HabitState();
+}
+
+class _HabitState extends State<Habit> {
+  bool _isChecked = false;
+
+  void _toggleCheckbox(bool? value) {
+    setState(() {
+      _isChecked = value!;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(value: _isChecked, onChanged: _toggleCheckbox),
+        Text(widget.habitName)
+      ],
     );
   }
 }
